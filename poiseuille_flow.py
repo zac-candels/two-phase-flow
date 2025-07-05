9677e7b2af3efacbedeaae41e4efad2eb0e01163
Zachary Candelaria <s2767572@ed.ac.uk>
2025-07-05 17:02:51 +0100

    (1) replaced fe.nabla_grad(v) with epsilon(v) where the other term was epsilon(u_star) or epsilon(u_n). (2) A sign error in the boundary pressure term. (3) deleted assembly of RHS vectors before timestep. It finally fucking works

--- poiseuille_flow.py
+++ poiseuille_flow.py
@@ -19,7 +19,7 @@ L_x = 4.0
 H = 1.0 # channel height
 nx = 10
 ny = nx
-mesh = fe.RectangleMesh(fe.Point(0,0), fe.Point(L_x, H), nx, ny)
+#mesh = fe.RectangleMesh(fe.Point(0,0), fe.Point(L_x, H), nx, ny)
 mesh = fe.UnitSquareMesh(16, 16)
 
 V = fe.VectorFunctionSpace(mesh, "P", 2)
@@ -57,11 +57,11 @@ p_jc = fe.Function(Q) # Just computed pressure
 
 
 # Expression used in variational forms
-U_cn = 0.5*( u_star + u_n )
 n = fe.FacetNormal(mesh)
-#mu = fe.Constant(mu)
-k = dt #fe.Constant(dt)
-#rho = fe.Constant(rho)
+mu = fe.Constant(mu)
+k = fe.Constant(dt)
+rho = fe.Constant(rho)
+f = fe.Constant( (0,0) )
 
 # Define strain-rate tensor
 def epsilon(u):
@@ -74,37 +74,33 @@ def sigma(u, p):
 
 # Bilinear and linear forms, step 1
 a1 = rho*(1/k)*fe.dot( u_star, v )*fe.dx\
-     + fe.inner( mu*epsilon(u_star), fe.nabla_grad(v) )*fe.dx\
+     + fe.inner( mu*epsilon(u_star), epsilon(v) )*fe.dx\
              - 0.5*mu*fe.dot( fe.nabla_grad(u_star)*n , v)*fe.ds\
                 
 L1 = rho*(1/k)*fe.dot(u_n,v)*fe.dx\
     - rho*fe.dot( fe.dot( u_n, fe.nabla_grad(u_n) ), v)*fe.dx\
-        + fe.dot(p_n*n, v)*fe.ds\
-            - fe.inner( mu*epsilon(u_n), fe.nabla_grad(v) )*fe.dx\
+        - fe.dot(p_n*n, v)*fe.ds\
+            - fe.inner( mu*epsilon(u_n), epsilon(v) )*fe.dx\
             + fe.inner( p_n*fe.Identity( len(u_n) ),\
-                             fe.nabla_grad(v) )*fe.dx\
-                    + 0.5*mu*fe.dot( fe.nabla_grad(u_n)*n , v)*fe.ds\
+                             epsilon(v) )*fe.dx\
+                    + 0.5*mu*fe.dot( fe.nabla_grad(u_n)*n , v)*fe.ds + fe.dot(f, v)*fe.dx
         
 
 # Bilinear and linear forms, step 2
 a2 = fe.dot(fe.nabla_grad(p_np1), fe.nabla_grad(q) )*fe.dx
 L2 = fe.dot(fe.nabla_grad(p_n), fe.nabla_grad(q) )*fe.dx\
-     - (rho/dt)*fe.div(u_star_jc)*q*fe.dx 
+     - (1/k)*fe.div(u_star_jc)*q*fe.dx 
     
 # # Bilinear and linear forms, step 3
-a3 = rho*(1/k)*fe.dot( u_np1, v )*fe.dx
-L3 = rho*fe.dot(u_star_jc, v)*fe.dx\
-     -dt*fe.dot( ( fe.nabla_grad(p_jc - p_n) ), v)*fe.dx
+a3 = fe.dot( u_np1, v )*fe.dx
+L3 = fe.dot(u_star_jc, v)*fe.dx\
+     -k*fe.dot( ( fe.nabla_grad(p_jc - p_n) ), v)*fe.dx
        
     
 # Assemble matrices
 A1 = fe.assemble(a1) 
 A2 = fe.assemble(a2)
 A3 = fe.assemble(a3)
-
-b1 = fe.assemble(L1)
-b2 = fe.assemble(L2)
-b3 = fe.assemble(L3)
 
 # Apply BCs to matrices
 [bc.apply(A1) for bc in bcu]
